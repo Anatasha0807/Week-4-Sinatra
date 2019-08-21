2ac1e564bb848472d0d94600f9de0f6a30cac9dd
Anatasha <anatashadyahia.88@gmail.com>
2019-08-21 17:32:29 +0800

    done game html

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -16,43 +16,67 @@ class ApplicationController < Sinatra::Base
   get '/level' do
     session['guessed_number'] = nil
     session['secret_number'] = nil
-
     erb :level
   end
 
-  post '/new_game' do
+  post '/new_game' do #1 start guess number
     session['level'] = params['game_level']
+    set_secret_number
+
+    session['total_chances'] = total_chances
+    session['counter'] = counter
 
     redirect to('/game')
   end
 
   get '/game' do
-    session['secret_number'] = secret_number(session['secret_number'])
     erb :game
   end
 
   post '/game' do
     session['guessed_number'] = params['guessed_number']
 
-      if session['guessed_number'].to_i == session['secret_number']
-        erb :win
+    if number_match
+      erb :win
+    else
+      session['counter'] += 1
+
+      if session['total_chances'] == session['counter']
+        erb :gameover
       else
         erb :game
       end
     end
   end
 
+  def text_try_again
+    ["Ops, wrong answer!","Close enough. Try again!","Almost there!"].sample
+  end
+
+private
   def secret_number(level)
     if level == 'easy'
       secret_number = rand(1..30)
     elsif level == 'medium'
       secret_number = rand(1..50)
-    else level == 'hard'
+    else
       secret_number = rand(1..100)
+    end
+  end
+
+  def number_match
+    session['guessed_number'].to_i == session['secret_number']
+  end
+
+  def set_secret_number
+    session['secret_number'] = secret_number(session['level'])
+  end
+
+  def total_chances
+    3
+  end
+
+  def counter
+    0
   end
 end
-
-#   def secret_number
-#     78
-#   end
-# end
